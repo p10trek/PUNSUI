462303bebf04fa2182d5954981e90a5d838fd64f
Marcin <ziewa91@wp.pl>
2021-01-09 17:49:46 +0100

    Basic features

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,12 +1,25 @@
 <template>
-<div class="row">
-  <h1>Score board</h1>
-</div>
+  <div class="row justify-content-center">
+    <h1>Score board</h1>
+    <div v-if="playersAreAlreadyDownloaded" class="col-12">
+      <h3 v-for="player in players" :key="player.id">{{player.nick + ": " + player.score}}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+
 export default {
-name: "ScoreBoard"
+  name: "ScoreBoard",
+  computed: {
+    ...mapGetters({
+      players: 'getPlayers'
+    }),
+    playersAreAlreadyDownloaded() {
+      return this.players !== null && this.players.length > 0
+    }
+  }
 }
 </script>
 
