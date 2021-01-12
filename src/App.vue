ee3f3c1666495429244e113fbfb650b7f538e7a9
sokolowski.m98 <sokolowski.m98@gmail.com>
2021-01-12 22:36:35 +0100

    PUNS-30

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view />
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+	computed: {
+		...mapGetters(["getUser"])
+	},
+	watch: {
+		getUser(newVal) {
+			if (Object.entries(newVal).length === 0) {
+				this.$router.push({ name: "login" });
+			} else {
+				this.connect();
+			}
+		}
+	}
+};
+</script>
