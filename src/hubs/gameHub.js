33a27dd04b681090c427da48ace11c79bee43df4
Najssky <sokolowski.m98@gmail.com>
2021-01-17 22:48:12 +0100

    general cleaning, scores handling

--- src/hubs/gameHub.js
+++ src/hubs/gameHub.js
@@ -1,155 +1,169 @@
-import {HubConnectionBuilder, LogLevel} from '@microsoft/signalr'
 // import { signalR } from '@microsoft/signalr'
-import JwtService from "@/common/jwt.service"
+import JwtService from "@/common/jwt.service";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 export default {
-    install(Vue) {
-        const gameHub = new Vue()
-        Vue.prototype.$gameHub = gameHub
+	install(Vue) {
+		const gameHub = new Vue();
+		Vue.prototype.$gameHub = gameHub;
 
-        // Provide methods to connect/disconnect from the SignalR hub
-        let connection = null
-        let startedPromise = null
-        let manuallyClosed = false
+		// Provide methods to connect/disconnect from the SignalR hub
+		let connection = null;
+		let startedPromise = null;
+		let manuallyClosed = false;
 
-        const url = process.env.VUE_APP_BASE_URL + 'gameHub';
+		const url = process.env.VUE_APP_BASE_URL + "gameHub";
 
-        Vue.prototype.startSignalR = () => {
-            connection = new HubConnectionBuilder()
-                .configureLogging(LogLevel.Debug)
-                .withUrl(url, {accessTokenFactory: JwtService.getAccessToken})
-                .build()
+		Vue.prototype.startSignalR = () => {
+			connection = new HubConnectionBuilder()
+				.configureLogging(LogLevel.Debug)
+				.withUrl(url, { accessTokenFactory: JwtService.getAccessToken })
+				.build();
 
-            // Forward hub events through the event, so we can listen for them in the Vue components
-            connection.on('PlayerJoined', (playerId) => {
-                console.log("plauer joined gamehub")
-                gameHub.$emit('player-joined', playerId)
-            })
+			connection.on("PlayerJoined", (playerId) => {
+				gameHub.$emit("player-joined", playerId);
+			});
 
-            connection.on('GameStarted', () => {
-                gameHub.$emit('game-started')
-            })
+			connection.on("Scoreboard", (scores) => {
+				gameHub.$emit("scores-received", scores);
+			});
 
-            connection.on('PlayerQuit', (playerId) => {
-                gameHub.$emit('player-quit', playerId)
-            })
+			connection.on("GameStarted", () => {
+				gameHub.$emit("game-started");
+			});
 
-            connection.on('GameEnded', () => {
-                gameHub.$emit('game-ended')
-            })
+			connection.on("PlayerQuit", (playerId) => {
+				gameHub.$emit("player-quit", playerId);
+			});
 
-            connection.on('PlayerGuessed', (nextPlayerId) => {
-                gameHub.$emit('player-guessed', nextPlayerId)
-            })
+			connection.on("GameEnded", () => {
+				gameHub.$emit("game-ended");
+			});
 
-            connection.on('SwitchPlayer', () => {
-                gameHub.$emit('switch-player')
-            })
+			connection.on("PlayerGuessed", (nextPlayerId) => {
+				gameHub.$emit("player-guessed", nextPlayerId);
+			});
 
-            connection.on('NewShowingPlayer', (playerId) => {
-                gameHub.$emit('new-showing-player', playerId)
-            })
+			connection.on("SwitchPlayer", () => {
+				gameHub.$emit("switch-player");
+			});
 
-            connection.on('PlayerScored', (nextPlayerId) => {
-                gameHub.$emit('player-scored', nextPlayerId)
-            })
+			connection.on("NewShowingPlayer", (playerId) => {
+				gameHub.$emit("new-showing-player", playerId);
+			});
 
-            connection.on('SendErrorMessage', (message) => {
-                gameHub.$emit('error-message', message)
-            })
+			connection.on("PlayerScored", (nextPlayerId) => {
+				gameHub.$emit("player-scored", nextPlayerId);
+			});
 
-            connection.on('FetchPasswords', (data) => {
-                gameHub.$emit('fetch-passwords', data)
-            })
+			connection.on("SendErrorMessage", (message) => {
+				gameHub.$emit("error-message", message);
+			});
 
-            // You need to call connection.start() to establish the connection but the client wont handle reconnecting for you!
-            // Docs recommend listening onclose and handling it there.
-            // This is the simplest of the strategies
-            function start() {
-                startedPromise = connection.start()
-                    .catch(err => {
-                        console.error('Failed to connect with hub', err)
-                        return new Promise((resolve, reject) => setTimeout(() => start().then(resolve).catch(reject), 5000))
-                    })
-                return startedPromise
-            }
+			connection.on("FetchPasswords", (data) => {
+				console.log(data);
+				gameHub.$emit("fetch-passwords", data);
+			});
 
-            connection.onclose(() => {
-                if (!manuallyClosed) start()
-            })
+			// You need to call connection.start() to establish the connection but the client wont handle reconnecting for you!
+			// Docs recommend listening onclose and handling it there.
+			// This is the simplest of the strategies
+			function start() {
+				startedPromise = connection.start().catch((err) => {
+					console.error("Failed to connect with hub", err);
+					return new Promise((resolve, reject) =>
+						setTimeout(
+							() =>
+								start()
+									.then(resolve)
+									.catch(reject),
+							5000,
+						),
+					);
+				});
+				return startedPromise;
+			}
 
-            // Start everything
-            manuallyClosed = false
-            start()
-        }
-        Vue.prototype.stopSignalR = () => {
-            if (!startedPromise) return
+			connection.onclose(() => {
+				if (!manuallyClosed) start();
+			});
 
-            manuallyClosed = true
-            return startedPromise
-                .then(() => connection.stop())
-                .then(() => {
-                    startedPromise = null
-                })
-        }
+			// Start everything
+			manuallyClosed = false;
+			start();
+		};
+		Vue.prototype.stopSignalR = () => {
+			if (!startedPromise) return;
 
-        // Provide methods for components to send messages back to server
-        // Make sure no invocation happens until the connection is established
-        gameHub.joinGame = (gameId) => {
-            if (!startedPromise) return
+			manuallyClosed = true;
+			return startedPromise
+				.then(() => connection.stop())
+				.then(() => {
+					startedPromise = null;
+				});
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('JoinGame', gameId))
-                .catch(console.error)
-        }
+		// Provide methods for components to send messages back to server
+		// Make sure no invocation happens until the connection is established
+		gameHub.joinGame = (gameId) => {
+			if (!startedPromise) return;
 
-        gameHub.quitGame = (gameId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() => connection.invoke("JoinGame", gameId))
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('QuitGame', gameId))
-                .catch(console.error)
-        }
+		gameHub.quitGame = (gameId) => {
+			if (!startedPromise) return;
 
-        gameHub.gameStart = (gameId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() => connection.invoke("QuitGame", gameId))
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('GameStart', gameId))
-                .catch(console.error)
-        }
+		gameHub.gameStart = (gameId) => {
+			if (!startedPromise) return;
 
-        gameHub.gameEnd = (gameId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() => connection.invoke("GameStart", gameId))
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('GameEnd', gameId))
-                .catch(console.error)
-        }
+		gameHub.gameEnd = (gameId) => {
+			if (!startedPromise) return;
 
-        gameHub.playerGuessed = (gameId, nextPlayerId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() => connection.invoke("GameEnd", gameId))
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('PlayerGuessed', gameId, nextPlayerId))
-                .catch(console.error)
-        }
+		gameHub.playerGuessed = (gameId, nextPlayerId) => {
+			if (!startedPromise) return;
 
-        gameHub.newShowingPlayer = (gameId, playerId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() =>
+					connection.invoke("PlayerGuessed", gameId, nextPlayerId),
+				)
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('NewShowingPlayer', gameId, playerId))
-                .catch(console.error)
-        }
+		gameHub.newShowingPlayer = (gameId, playerId) => {
+			if (!startedPromise) return;
 
-        gameHub.removeFromGameGroup = (gameId, playerId) => {
-            if (!startedPromise) return
+			return startedPromise
+				.then(() =>
+					connection.invoke("NewShowingPlayer", gameId, playerId),
+				)
+				.catch(console.error);
+		};
 
-            return startedPromise
-                .then(() => connection.invoke('RemoveFromGameGroup', gameId, playerId))
-                .catch(console.error)
-        }
-
-    }
-}
+		gameHub.removeFromGameGroup = (gameId, playerId) => {
+			if (!startedPromise) return;
+			return startedPromise
+				.then(() =>
+					connection.invoke("RemoveFromGameGroup", gameId, playerId),
+				)
+				.catch(console.error);
+		};
+	},
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="container-fluid">
-      <div class="row">
-        <div class="col-12 text-right">
-          <button @click="logout" class="btn-dark">Log out</button>
-        </div>
-    </div>
-    <div class="row">
-      <div class="col-12 col-md-6">
-        <GameBoard></GameBoard>
-      </div>
-      <div class="col-6 col-md-3">
-        <ScoreBoard></ScoreBoard>
-      </div>
-      <div class="col-6 col-md-3" v-if="user !== null">
-        <Room></Room>
-      </div>
-    </div>
-  </div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 text-right">
+				<button @click="logout" class="btn-dark">Log out</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<GameBoard></GameBoard>
+			</div>
+			<div class="col-6 col-md-3">
+				<ScoreBoard></ScoreBoard>
+			</div>
+			<div class="col-6 col-md-3" v-if="user !== null">
+				<Room></Room>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -24,41 +24,46 @@
 import GameBoard from "@/components/GameBoard";
 import Room from "@/components/Room";
 import ScoreBoard from "@/components/ScoreBoard";
-import {LOGOUT, FETCH_USER, FETCH_ROOMS, FETCH_GAMES} from "@/store/actions.type";
-import { mapGetters } from 'vuex';
+import {
+	LOGOUT,
+	FETCH_USER,
+	FETCH_ROOMS,
+	FETCH_GAMES,
+} from "@/store/actions.type";
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Home',
-  components: {
-    GameBoard,
-    Room,
-    ScoreBoard
-  },
-  methods: {
-    logout(){
-      this.$store.dispatch(LOGOUT).then(() => this.$router.push({ name : "login"}))
-    },
-  },
-  computed: {
-    ...mapGetters({
-      isAuthenticated : 'isAuthenticated',
-      user: 'getUser'
-    })
-  },
-  created() {
-    if (!this.isAuthenticated){
-      //this.$router.push({ name : "login" })
-      console.log("isAuthenticate na false")
-    }
-    this.$gameHub.stopSignalR();
-    this.$store.dispatch(FETCH_USER);
-    this.$store.dispatch(FETCH_ROOMS);
-    this.$store.dispatch(FETCH_GAMES);
-    this.$gameHub.startSignalR();
-  },
-  beforeDestroy() {
-    this.$gameHub.stopSignalR();
-  }
-
-}
+	name: "Home",
+	components: {
+		GameBoard,
+		Room,
+		ScoreBoard,
+	},
+	methods: {
+		logout() {
+			this.$store
+				.dispatch(LOGOUT)
+				.then(() => this.$router.push({ name: "login" }));
+		},
+	},
+	computed: {
+		...mapGetters({
+			isAuthenticated: "isAuthenticated",
+			user: "getUser",
+		}),
+	},
+	created() {
+		if (!this.isAuthenticated) {
+			this.$router.push({ name: "login" });
+		}
+		this.$gameHub.stopSignalR();
+		this.$store.dispatch(FETCH_USER);
+		this.$store.dispatch(FETCH_ROOMS);
+		this.$store.dispatch(FETCH_GAMES);
+		this.$gameHub.startSignalR();
+	},
+	beforeDestroy() {
+		this.$gameHub.stopSignalR();
+	},
+};
 </script>
